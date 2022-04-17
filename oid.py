50f900a97d0bbec68433b9f1dc5dc6b75d370905
AissAiss <lucasaissaoui201@gmail.com>
2022-04-17 12:18:34 +0200

    Dernier commit

--- oid.py
+++ oid.py
@@ -1,10 +1,17 @@
-from tkinter import END, RAISED, Tk, Label, Entry, Button, Text, ttk
+from ctypes import alignment
+from tkinter import ANCHOR, END, RAISED, Tk, Label, Entry, Button, Text, ttk, PhotoImage
 from numpy import source
 
 from rdflib import Graph
 from rdflib import URIRef
 from rdflib.namespace import RDF
 from rdflib.namespace import FOAF
+
+import matplotlib.pyplot as plt
+import numpy as np
+from strsimpy.normalized_levenshtein import NormalizedLevenshtein
+import os
+
 
 source = ""
 target = ""
@@ -13,10 +20,13 @@ diff = ""
 inter = ""
 xor = ""
 
+X_BOX = 85
+Y_BOX = 15
+
 
 root = Tk()
 #root.attributes("-fullscreen", True)
-root.geometry("1300x800")
+root.geometry("1900x750")
 
 # Entry 
 label_source = Label(text="Fichier source : ", anchor='w')
@@ -107,15 +117,15 @@ bilan_button = Button(root, text="Bilan", command=bilan)
 bilan_button.grid(row=2, column=2)
 
 # Output
-source_output = Text(width=60, height=30, border=4, relief=RAISED)
+source_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 source_output.insert(END, "")
 source_output.grid(row=3, column=0)
 
-target_output = Text(width=60, height=30, border=4, relief=RAISED)
+target_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 target_output.insert(END, "")
 target_output.grid(row=3, column=1)
 
-bilan_output = Text(width=60, height=10, border=4, relief=RAISED)
+bilan_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 bilan_output.insert(END, "")
 bilan_output.grid(row=3, column=2)
 
@@ -199,18 +209,140 @@ analyse_button = Button(root, text="Analyse", command=analyse)
 analyse_button.grid(row=8, column=0)
 
 
-subjects_output = Text(width=60, height=30, border=4, relief=RAISED)
+subjects_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 subjects_output.insert(END, "")
 subjects_output.grid(row=9, column=0)
 
-predicates_output = Text(width=60, height=30, border=4, relief=RAISED)
+predicates_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 predicates_output.insert(END, "")
 predicates_output.grid(row=9, column=1)
 
-objects_output = Text(width=60, height=30, border=4, relief=RAISED)
+objects_output = Text(width=X_BOX, height=Y_BOX, border=4, relief=RAISED)
 objects_output.insert(END, "")
 objects_output.grid(row=9, column=2)
 
+
+label_graphe = Label(text="Graphe : ", anchor='w')
+label_graphe.grid(row=10, column=0, sticky='e')
+
+liste_graphe = ttk.Combobox(root, values=listeGraph)
+liste_graphe.current(0)
+liste_graphe.grid(row=10, column=1, sticky='w')
+
+label_triplet = Label(text="Triplet : ", anchor='w')
+label_triplet.grid(row=11, column=0, sticky='e')
+
+liste_triplet = ttk.Combobox(root, values=["Sujets", "Prédicats", "Objets"])
+liste_triplet.current(0)
+liste_triplet.grid(row=11, column=1, sticky='w')
+
+label_uri = Label(text="URI : ", anchor='w')
+label_uri.grid(row=12, column=0, sticky='e')
+
+entry_uri = Entry(width=60)
+entry_uri.insert(END, "http://...")
+entry_uri.grid(row=12, column=1, sticky='w')
+
+label_precision = Label(text="Précision : ", anchor='w')
+label_precision.grid(row=13, column=0, sticky='e')
+
+entry_precision = Entry(width=25)
+entry_precision.insert(END, "10")
+entry_precision.grid(row=13, column=1, sticky='w')
+
+def simCompare(graph, uri, precision):
+    subjects = {}
+    predicates = {}
+    objects = {}
+
+    subjectsOrdered = {}
+    predicatesOrdered = {}
+    objectsOrdered = {}
+
+    for s, p, o in graph.triples((None,  None, None)):
+        s = str(s)
+        p = str(p)
+        o = str(o)
+        
+        if s in subjects :
+            subjects[s] += 1
+        else :
+            subjects[s] = 0 
+            
+        if p in predicates :
+            predicates[p] += 1
+        else :
+            predicates[p] = 0 
+            
+        if o in objects :
+            objects[o] += 1
+        else :
+            objects[o] = 0 
+
+    for i in sorted(subjects, key=subjects.get, reverse=True):
+        subjectsOrdered[i] = subjects[i]
+        
+    for i in sorted(predicates, key=predicates.get, reverse=True):
+        predicatesOrdered[i] = predicates[i]
+
+    for i in sorted(objects, key=objects.get, reverse=True):
+        objectsOrdered[i] = objects[i]
+
+
+    normalized_levenshtein = NormalizedLevenshtein()
+    sameAs = 0
+    x = []
+    y = []
+
+    tripletSelected = str(liste_triplet.get())
+    #dimTriplet = subjectsOrdered
+
+    if tripletSelected == "Sujets": 
+        dimTriplet = subjectsOrdered
+    elif tripletSelected == "Prédicats": 
+        dimTriplet = predicatesOrdered
+    elif tripletSelected == "Objets": 
+        dimTriplet = objectsOrdered
+
+    for i in np.linspace(0,1,precision,endpoint=True):
+        for t in dimTriplet: 
+            if normalized_levenshtein.distance(str(uri), str(t)) < i: 
+                sameAs += 1
+
+        x.append(i)
+        y.append(sameAs)
+        sameAs=0
+    
+    fig = plt.figure()
+    ax = plt.axes()
+    plt.xlabel("Seuil")
+    plt.ylabel("Nombre d'ocurrence")
+    ax.plot(x, y)
+
+    plt.savefig('autoSave.png')
+
+    os.system("python plot.py")
+    
+
+def calculer(): 
+    graphSelected = str(liste_graphe.get())
+
+    if graphSelected == "Source": 
+        simCompare(graph=source, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+    elif graphSelected == "Cible": 
+        simCompare(graph=target, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+    elif graphSelected == "Union": 
+        simCompare(graph=add, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+    elif graphSelected == "Difference": 
+        simCompare(graph=diff, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+    elif graphSelected == "Intersection": 
+        simCompare(graph=inter, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+    elif graphSelected == "XOR": 
+        simCompare(graph=xor, uri=str(entry_uri.get()), precision=int(entry_precision.get()))
+
+    
+calculer_button = Button(root, text="Calculer", command=calculer)
+calculer_button.grid(row=14, column=0, sticky='e')
 
 
 root.mainloop() 
